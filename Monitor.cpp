8fb9570e2379b6694c991ad648dfa0aa0b3294d6
Pawel Lampe <scony@sconysoft.com>
2014-04-28 00:50:42 +0200

    Mutex -> Moniotor

--- Monitor.cpp
+++ Monitor.cpp
@@ -4,14 +4,17 @@
 #include <unistd.h>
 
 #include "Monitor.hpp"
-#include "Mutex.hpp"
 
 using namespace std;
+
+Monitor * Monitor::instance = NULL;
 
 Monitor::Monitor() :
   pvm(Pvm::getInstance())
 {
-
+  instance = this;
+  timestamp = 0;
+  responses = 0;
 }
 
 Monitor::~Monitor()
@@ -29,12 +32,66 @@ bool Monitor::done()
   return pvm.vTids == vDone;
 }
 
+bool Monitor::critical()
+{
+  cout << responses << "::" << pvm.vTids.size() << "::" << queue.size() << endl;
+  if(responses == pvm.vTids.size() && queue.front().id == pvm.tid)
+    return true;
+  return false;
+}
+
 void Monitor::recv()
 {
   int bufid;
 
-  // mutex recv
-  Mutex::getInstance().recv();
+  // MUTEX_REQUEST
+  bufid = pvm_nrecv(-1,MUTEX_REQUEST);
+  if(bufid > 0)
+    {
+      // add to queue & sort it
+      int inMsg[2];
+      pvm_upkint(inMsg,2,1);
+      queue.push_back(Element(inMsg[0],inMsg[1]));
+      queue.sort();
+
+      // update lamport clock
+      timestamp = (timestamp > inMsg[1] ? timestamp : inMsg[1]) + 1;
+
+      // send response
+      int outMsg[2] = { pvm.tid, timestamp };
+      pvm_psend(inMsg[0],MUTEX_RESPONSE,outMsg,2,PVM_INT);
+    }
+
+  // MUTEX_RESPONSE
+  bufid = pvm_nrecv(-1,MUTEX_RESPONSE);
+  if(bufid > 0)
+    {
+      // increment responses
+      int msg[2];
+      pvm_upkint(msg,2,1);
+      responses++;
+
+      // update lamport clock
+      timestamp = (timestamp > msg[1] ? timestamp : msg[1]) + 1;
+    }
+
+  // MUTEX_RELEASE
+  bufid = pvm_nrecv(-1,MUTEX_RELEASE);
+  if(bufid > 0)
+    {
+      // drop him from queue
+      int msg[2];
+      pvm_upkint(msg,2,1);
+      for(list<Element>::iterator i = queue.begin(); i != queue.end(); i++)
+	if(i->id == msg[0])
+	  {
+	    queue.erase(i);
+	    break;
+	  }
+
+      // update lamport clock
+      timestamp = (timestamp > msg[1] ? timestamp : msg[1]) + 1;
+    }
 
   // MONITOR_DONE
   bufid = pvm_nrecv(-1,MONITOR_DONE);
@@ -46,15 +103,76 @@ void Monitor::recv()
     }
 
   // dont waste CPU
-  // usleep(100000);
+  usleep(100000);
+}
+
+void Monitor::lock()
+{
+  cout << "lock\n";
+
+  // increment lamport clock
+  timestamp++;
+
+  // add myself to queue & sort
+  queue.push_back(Element(pvm.tid,timestamp));
+  queue.sort();
+
+  // send MUTEX_REQUEST to all
+  int msg[2] = { pvm.tid, timestamp };
+  for(vector<int>::iterator i = pvm.vTids.begin(); i != pvm.vTids.end(); i++)
+    pvm_psend(*i,MUTEX_REQUEST,msg,2,PVM_INT);
+
+  // while not in critical section wait for messages
+  while(!critical())
+    recv();
+}
+
+void Monitor::unlock()
+{
+  if(critical())
+    {
+      cout << "unlock\n";
+
+      // increment lamport clock
+      timestamp++;
+
+      // send MUTEX_RELEASE to all
+      int msg[2] = { pvm.tid, timestamp };
+      for(vector<int>::iterator i = pvm.vTids.begin(); i != pvm.vTids.end(); i++)
+	pvm_psend(*i,MUTEX_RELEASE,msg,2,PVM_INT);
+
+      // drop myself from queue
+      for(list<Element>::iterator i = queue.begin(); i != queue.end(); i++)
+	if(i->id == pvm.tid)
+	  {
+	    queue.erase(i);
+	    break;
+	  }
+
+      // clear responses
+      responses = 0;
+    }
 }
 
 Monitor::_export::_export()
 {
-  Mutex::getInstance().lock();
+  Monitor::instance->lock();
 }
 
 Monitor::_export::~_export()
 {
-  Mutex::getInstance().unlock();
+  Monitor::instance->unlock();
+}
+
+Monitor::Element::Element(int id, int timestamp)
+{
+  this->id = id;
+  this->timestamp = timestamp;
+}
+
+bool Monitor::Element::operator<(Element &e)
+{
+  if(timestamp != e.timestamp)
+    return timestamp < e.timestamp;
+  return id < e.id;
 }
--- Monitor.hpp
+++ Monitor.hpp
@@ -1,15 +1,22 @@
 #ifndef MONITOR_HPP
 #define MONITOR_HPP
 
+#include <list>
+
 #include "Pvm.hpp"
 
 #define MONITOR_DONE 1
+
+#define MUTEX_REQUEST 10
+#define MUTEX_RESPONSE 11
+#define MUTEX_RELEASE 12
 
 #define EXPORT _export _exp;
 
 class Monitor
 {
 protected:
+  static Monitor * instance;
   Pvm & pvm;
 
   Monitor();
@@ -34,10 +41,27 @@ protected:
   // };
 
 private:
+  int timestamp;
+  int responses;
   std::vector<int> vDone;
 
-  bool done();
+  void lock();
+  void unlock();
   void recv();
+
+  bool done();
+  bool critical();
+
+  class Element
+  {
+  public:
+    int id;
+    int timestamp;
+    Element(int id, int timestamp);
+    bool operator<(Element &e);
+  };
+
+  std::list<Element> queue;
 };
 
 #endif
